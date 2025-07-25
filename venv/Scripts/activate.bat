735324d57119486188a65fde9555f45011e8cb1e
Havish Patel <25ateha@goodnews.vic.edu.au>
2025-07-25 15:05:45 +1000

    Fix login template and css, fixed error involving forgot password as it did not esist

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\25ateha\rp_flask_board\venv
+set VIRTUAL_ENV=C:\Users\25ateha\RP_flask_board\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
